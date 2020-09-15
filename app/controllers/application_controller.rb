cf7c23198a05c0e810f03630f5876d0dbda7078b
tbanish <tombanish@gmail.com>
2020-09-15 12:32:34 -0400

    enable sessions and set sessions secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :sessions_secret, "secret"
   end
 
 end
