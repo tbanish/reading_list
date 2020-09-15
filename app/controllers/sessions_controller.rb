bb21a235f960d234fa67bfcf842b85cfd62b2f30
tbanish <tombanish@gmail.com>
2020-09-15 15:07:39 -0400

    build post '/signup' route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,16 @@ class SessionsController < ApplicationController
   get '/signup' do
     erb :'/sessions/signup'
   end
+
+  post '/signup' do
+    binding.pry
+    if params[:name] != "" && params[:email] != "" && params[:password] != "" && !User.find_by(name: params[:name])
+      @user = User.create(params)
+      session[:id] = @user.id
+      redirect "/users/#{@user.id}"
+    else
+      redirect "/signup"
+    end
+  end
+
 end
