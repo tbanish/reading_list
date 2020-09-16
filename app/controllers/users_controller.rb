8143b1cabc0d15f765d407a53531529191109fde
tbanish <tombanish@gmail.com>
2020-09-16 14:16:17 -0400

    add code so that user can only access their own user show page if logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
 
   get '/users/:id' do
-    @user = User.find_by(params)
-    session[:id] = @user.id
-    erb :"/users/show"
+    @user = User.find_by(id: params[:id])
+    if !logged_in?
+      redirect "/login"
+    elsif logged_in? && @user.id == current_user.id
+      erb :"/users/show"
+    else
+      redirect "/users/#{current_user.id}"
+    end
   end
 
 end
