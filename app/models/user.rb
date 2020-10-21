85a4413ddc4f893019065c3528cf4a8a2c20a040
tbanish <tombanish@gmail.com>
2020-10-21 13:47:44 -0400

    add functionality to allow user to view number of books on their list from home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,18 @@
 class User < ActiveRecord::Base
   has_many :books
   has_secure_password
+
+  def number_of_books
+    self.books.size
+  end
+
+  def book_list_stats
+    if number_of_books == 0
+      "You do not have any books on your list."
+    elsif number_of_books == 1
+      "You have 1 book on your list."
+    elsif number_of_books > 1
+      "You have #{self.number_of_books} books on your list."
+    end
+  end
 end
