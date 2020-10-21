59990ef215dae79155a73b2ce2f745c7492f1a8f
tbanish <tombanish@gmail.com>
2020-10-21 13:54:13 -0400

    build empty_list_message and refactor book_list_stats to call empty_list_message

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
 
   def book_list_stats
     if number_of_books == 0
-      "You do not have any books on your list."
+      empty_list_message
     elsif number_of_books == 1
       "You have 1 book on your list."
     elsif number_of_books > 1
@@ -24,6 +24,10 @@ class User < ActiveRecord::Base
     elsif number_of_books == 0
       empty_list_message
     end
+  end
+
+  def empty_list_message
+    "You do not have any books on your list."
   end
 
 end
