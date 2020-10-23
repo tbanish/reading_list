ecd95e4c525ff56a88e77dc3ecb50eaeb5f7f599
tbanish <tombanish@gmail.com>
2020-10-23 17:55:44 -0400

    add flash message when user tries to access unauthorized user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
       flash[:message] = "You need to log in to view this page."
       redirect "/login"
     elsif (logged_in? && @user == nil) || (logged_in? && @user.id != current_user.id)
+      flash[:message] = "Click the links above to navigate the site."
       redirect "/users/#{current_user.id}"
     elsif logged_in? && @user.id == current_user.id
       erb :"/users/show"
