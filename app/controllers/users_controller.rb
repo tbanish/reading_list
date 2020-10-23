ff2c02a8dee195fa3c7cf97171a6862d286d95c4
tbanish <tombanish@gmail.com>
2020-10-23 17:53:00 -0400

    add flash message when logged out user tries to access user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   get '/users/:id' do
     @user = User.find_by(id: params[:id])
     if !logged_in?
+      flash[:message] = "You need to log in to view this page."
       redirect "/login"
     elsif (logged_in? && @user == nil) || (logged_in? && @user.id != current_user.id)
       redirect "/users/#{current_user.id}"
