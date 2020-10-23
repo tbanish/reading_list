4bb0eaf69264f31b34dd2a28bd827dc89efc6bf4
tbanish <tombanish@gmail.com>
2020-10-23 17:21:53 -0400

    add flash message when logged out user tries to access books index page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
   get '/books' do
     if !logged_in?
+      flash[:message] = "You need to log in to view this page."
       redirect "/login"
     else
       @user = User.find_by(id: current_user.id)
