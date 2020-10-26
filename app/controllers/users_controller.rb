1d97ff2ba05cb4c0c6bc769e339700521b5ef370
tbanish <tombanish@gmail.com>
2020-10-26 19:35:07 -0400

    refactor and add helper method user_exists to get '/users/:id route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     if !logged_in?
       log_in_to_view_page
       redirect "/login"
-    elsif (logged_in? && @user == nil) || (logged_in? && @user.id != current_user.id)
+    elsif (logged_in? && !user_exists) || (logged_in? && @user != current_user)
       flash[:message] = "Click the links above to navigate the site."
       redirect "/users/#{current_user.id}"
     elsif logged_in? && @user.id == current_user.id
